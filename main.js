66767b70aeb7ea9708fbaf94baa29a17aff0b786
Jian Liew <jianloongliew@gmail.com>
2022-10-27 11:04:34 +1100

    Added loading bar for creation of word cloud

--- main.js
+++ main.js
@@ -5,84 +5,15 @@ window.onload = (event) => {
     const button = document.getElementById("run");
     const tfIDF = document.getElementById("tfidf");
     const downloadButton = document.getElementById("download");
+    const runningButton = document.getElementById("running");
 
     const worker = new Worker("./worker/worker.js" + '?' + Math.random());
-
-    const generateCloud = (
-      text,
-      {
-        size = (group) => group.length, // Given a grouping of words, returns the size factor for that word
-        word = (d) => d, // Given an item of the data array, returns the word
-        marginTop = 0, // top margin, in pixels
-        marginRight = 0, // right margin, in pixels
-        marginBottom = 0, // bottom margin, in pixels
-        marginLeft = 0, // left margin, in pixels
-        width = undefined, // outer width, in pixels
-        height = undefined, // outer height, in pixels
-        maxWords = 250, // maximum number of words to extract from the text
-        fontFamily = "sans-serif", // font family
-        fontScale = 15, // base font size
-        padding = 0, // amount of padding between the words (in pixels)
-        rotate = 0, // a constant or function to rotate the words
-        invalidation, // when this promise resolves, stop the simulation
-        colourScheme = "d3.schemeCategory10",
-        spiral = archimedeanSpiral
-      } = {}
-    ) => {
-      const words =
-        typeof text === "string" ? text.split(/\W+/g) : Array.from(text);
-      const data = d3
-        .rollups(words, size, (w) => w)
-        .sort(([, a], [, b]) => d3.descending(a, b))
-        .slice(0, maxWords)
-        .map(([key, size]) => ({ text: word(key), size }));
-
-      // This is for the colour scheme
-      getColourSchemeDomain(colourScheme, data);
-
-      const svg = d3
-        .create("svg")
-        .attr("viewBox", [0, 0, width, height])
-        .attr("font-family", fontFamily)
-        .attr("text-anchor", "middle")
-        .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
-
-      const g = svg
-        .append("g")
-        .attr("transform", `translate(${marginLeft},${marginTop})`);
-
-
-
-      const cloud = d3.layout
-        .cloud()
-        .size([
-          width - marginLeft - marginRight,
-          height - marginTop - marginBottom,
-        ])
-        .words(data)
-        .spiral(spiral)
-        .padding(padding)
-        .rotate(rotate)
-        .font(fontFamily)
-        .fontSize((d) => Math.sqrt(d.size) * fontScale)
-        .on("word", ({ size, x, y, rotate, text, color }) => {
-          g.append("text")
-            .transition()
-            .duration(500)
-            .style("fill", color)
-            .attr("font-size", size)
-            .attr("transform", `translate(${x},${y}) rotate(${rotate})`)
-            .text(text);
-        });
-
-      cloud.start();
-
-      return svg.node();
-    };
 
     button.addEventListener("click", () => {
 
       downloadButton.classList.add("d-none");
+      runningButton.classList.remove("d-none");
+      button.classList.add("d-none");
 
       const svg = d3.select("svg");
       svg.selectAll("*").remove();
@@ -100,8 +31,6 @@ window.onload = (event) => {
       const endAngle = document.getElementById("endAngle").value;
       
       const selectedSpiral = document.querySelector('input[name="spiral"]:checked').value;
-
-
 
       if (textArea == undefined || textArea.value.length == 0)
         return;
@@ -136,6 +65,9 @@ window.onload = (event) => {
         d3.select("#cloud").node().appendChild(a);
 
         downloadButton.classList.remove("d-none");
+        button.classList.remove("d-none");
+        runningButton.classList.add("d-none");
+        
       };
     });
 
@@ -178,3 +110,82 @@ const getColourSchemeDomain = (colourSchemeString, data) => {
   }
   return data;
 }
+
+const validations = () => {
+  const textArea = document.getElementById("inputString");
+
+
+
+}
+
+const generateCloud = (
+  text,
+  {
+    size = (group) => group.length, // Given a grouping of words, returns the size factor for that word
+    word = (d) => d, // Given an item of the data array, returns the word
+    marginTop = 0, // top margin, in pixels
+    marginRight = 0, // right margin, in pixels
+    marginBottom = 0, // bottom margin, in pixels
+    marginLeft = 0, // left margin, in pixels
+    width = undefined, // outer width, in pixels
+    height = undefined, // outer height, in pixels
+    maxWords = 250, // maximum number of words to extract from the text
+    fontFamily = "sans-serif", // font family
+    fontScale = 15, // base font size
+    padding = 0, // amount of padding between the words (in pixels)
+    rotate = 0, // a constant or function to rotate the words
+    invalidation, // when this promise resolves, stop the simulation
+    colourScheme = "d3.schemeCategory10",
+    spiral = archimedeanSpiral
+  } = {}
+) => {
+  const words =
+    typeof text === "string" ? text.split(/\W+/g) : Array.from(text);
+  const data = d3
+    .rollups(words, size, (w) => w)
+    .sort(([, a], [, b]) => d3.descending(a, b))
+    .slice(0, maxWords)
+    .map(([key, size]) => ({ text: word(key), size }));
+
+  // This is for the colour scheme
+  getColourSchemeDomain(colourScheme, data);
+
+  const svg = d3
+    .create("svg")
+    .attr("viewBox", [0, 0, width, height])
+    .attr("font-family", fontFamily)
+    .attr("text-anchor", "middle")
+    .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
+
+  const g = svg
+    .append("g")
+    .attr("transform", `translate(${marginLeft},${marginTop})`);
+
+
+
+  const cloud = d3.layout
+    .cloud()
+    .size([
+      width - marginLeft - marginRight,
+      height - marginTop - marginBottom,
+    ])
+    .words(data)
+    .spiral(spiral)
+    .padding(padding)
+    .rotate(rotate)
+    .font(fontFamily)
+    .fontSize((d) => Math.sqrt(d.size) * fontScale)
+    .on("word", ({ size, x, y, rotate, text, color }) => {
+      g.append("text")
+        .transition()
+        .duration(500)
+        .style("fill", color)
+        .attr("font-size", size)
+        .attr("transform", `translate(${x},${y}) rotate(${rotate})`)
+        .text(text);
+    });
+
+  cloud.start();
+
+  return svg.node();
+};
